cdbbe8aa33f6c0a780f25c27b721b85aadd82036
stefan6 <stefan@osnabrueck.freifunk.net>
2016-03-12 09:58:50 +0100

    add initial siteconf

---
+++ site.mk
@@ -0,0 +1,52 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-core \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-mesh-vpn-fastd \
+	gluon-next-node \
+	gluon-radvd \
+	gluon-respondd \
+	gluon-setup-mode \
+	gluon-status-page \
+	gluon-legacy \
+	haveged \
+	iptables \
+	iwinfo \
+	ffnw-banner \
+	libwlocate \
+	lwtrace \
+	ffnw-node-info \
+	ffnw-config-mode-geo-location \
+	ffnw-config-mode-contact-info
+
+ifeq ($(GLUON_TARGET),x86-generic)
+	# support the usb stack on x86 devices
+	# # and add a few common USB NICs
+GLUON_SITE_PACKAGES += \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-dm9601-ether
+endif
+
+DEFAULT_GLUON_RELEASE := 0.8+exp$(shell date '+%Y%m%d')
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+
+GLUON_LANGS ?= de en
